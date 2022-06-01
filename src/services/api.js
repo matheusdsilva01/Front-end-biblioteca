257b73b62955fe9fc257b3acc808d273fad7a1ca
Matheus Silva <ms25022003@gmail.com>
2022-06-01 18:35:30 -0300

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const respClient = axios.create({
-  baseURL: 'https://biblioteca-dev.herokuapp.com',
+  baseURL: 'https://biblioteca-devchallenge-production.up.railway.app',
 })
 
 export default respClient;
